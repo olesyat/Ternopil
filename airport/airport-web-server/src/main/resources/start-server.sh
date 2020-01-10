9125c10a81a41780894755a50c1b8702891c9e24
dariya24 <darynareshetukha@gmail.com>
2020-01-10 22:15:38 +0200

    Final

--- airport/airport-web-server/src/main/resources/start-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -server -Xms512m -Xmx2048m -XX:+UseG1GC -XX:+UseCompressedOOps -XX:+UseTLAB -Djava.system.class.loader=ua.com.fielden.platform.classloader.TgSystemClassLoader -cp .:lib/* helsinki.webapp.Start application.properties
